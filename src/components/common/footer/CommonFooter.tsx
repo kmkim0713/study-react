e65f20d650e5b570c275ebf5d43bf6b4fb700178
kmkim0713 <kmkimworkwork@gmail.com>
2024-11-29 16:48:30 +0900

    시멘틱 태그 변경

--- src/components/common/footer/CommonFooter.tsx
+++ src/components/common/footer/CommonFooter.tsx
@@ -1,7 +1,7 @@
 import styles from './CommonFooter.module.scss'
 
 function CommonFooter() {
-    return <div className={styles.footer}>
+    return <footer className={styles.footer}>
         <div className={styles.pagination}>
             <button className={styles.pagination__button}>
                 <img src="src/assets/icons/icon-arrowLeft.svg" alt="" />
@@ -12,7 +12,7 @@ function CommonFooter() {
                 <img src="src/assets/icons/icon-arrowRight.svg" alt="" />
             </button>
         </div>
-    </div>;
+    </footer>;
 }
 
 export default CommonFooter;
